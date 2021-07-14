26d12d76cf7b6e7a6863b8998ec53e8fab4f754c
Spencer <Spencer.Taber@yahoo.com>
2021-07-14 14:41:40 +0100

    second attempt at title change

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
-      <h1>Spencer's Hit Parade</h1>
+      <h1>Spencers Hit Parade</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
